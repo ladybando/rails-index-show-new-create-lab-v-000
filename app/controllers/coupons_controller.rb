7822b678931ed2f78e1647bbd0d99952ffae0854
Ayana Bando <ayanaflax@yahoo.com>
2019-01-28 21:21:40 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,12 +7,12 @@ class CouponsController < ApplicationController
     
     def new
     end
+    
     def create
         @coupon = Coupon.new(store: params[:coupon][:store], coupon_code: params[:coupon][:coupon_code])
         @coupon.save
         redirect_to coupon_path(@coupon)
     end
-
 
     def show
         @coupon = Coupon.find(params[:id])
